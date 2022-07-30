659b713a99574fbb393e05768e80494eecca9170
bzhang102 <yuxiangbrandonzhang@gmail.com>
2022-07-29 23:13:24 -0700

    remove unused fields from interface

--- src/main/java/cs56/group2/contact/client/model/Contact.java
+++ src/main/java/cs56/group2/contact/client/model/Contact.java
@@ -1,20 +1,15 @@
 package cs56.group2.contact.client.model;
 
 public interface Contact {
-    String firstName = null;
-    String lastName = null;
-    String middleName = null;
-    String email = null;
-    String cell = null;
-
-    public String getFirstName();
-    public void setFirstName(String firstName);
-    public String getLastName();
-    public void setLastName(String lastName);
-    public String getMiddleName();
-    public void setMiddleName(String middleName);
-    public String getEmail();
-    public void setEmail(String email);
-    public String getCell();
-    public void setCell(String cell);
+    int getId();
+    void setId(int id);
+    String getFirstName();
+    void setFirstName(String firstName);
+    String getLastName();
+    void setLastName(String lastName);
+    String getMiddleName();
+    void setMiddleName(String middleName);
+    String getEmail();
+    void setEmail(String email);
+    String getCell();void setCell(String cell);
 }
